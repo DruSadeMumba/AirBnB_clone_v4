e7751bcd847ad62fe9f14b08be3c759a2e37d4b3
Celse13 <117368635+Celse13@users.noreply.github.com>
2024-01-14 22:31:18 +0200

    Correct errors

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,56 +1,56 @@
 $(document).ready(() => {
-  const selectedAmenities = {};
-  $('input[type="checkbox"]').change(function () {
-    const amenityId = $(this).data('id');
-    const amenityName = $(this).data('name');
-    if ($(this).is(':checked')) {
-      selectedAmenities[amenityId] = amenityName;
-    } else {
-      delete selectedAmenities[amenityId];
-    }
-    updateAmenitiesList();
-  });
-
-  const updateAmenitiesList = () => {
-    const amenitiesList = Object.values(selectedAmenities).join(', ');
-    $('.popover h4').text(amenitiesList);
-  };
-
-  $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
-    } else {
-      $('#api_status').removeClass('available');
-    }
-  });
-
-  $('button').click(function () {
-    $.ajax({
-      type: 'POST',
-      url: 'http://0.0.0.0:5001/api/v1/places_search/',
-      contentType: 'application/json',
-      data: JSON.stringify({ amenities: Object.keys(selectedAmenities) }),
-      success: (data) => {
-        $('section.places').empty();
-        for (const place of data) {
-          $('section.places').append(`
-            <article>
-              <div class="title_box">
-                <h2>${place.name}</h2>
-                <div class="price_by_night">$${place.price_by_night}</div>
-              </div>
-              <div class="information">
-                <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
-                <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
-                <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
-              </div>
-              <div class="description">
-                ${place.description}
-              </div>
-            </article>
-          `);
-        }
+    const selectedAmenities = {};
+    $('input[type="checkbox"]').change(function () {
+      const amenityId = $(this).data('id');
+      const amenityName = $(this).data('name');
+      if ($(this).is(':checked')) {
+        selectedAmenities[amenityId] = amenityName;
+      } else {
+        delete selectedAmenities[amenityId];
+      }
+      updateAmenitiesList();
+    });
+  
+    const updateAmenitiesList = () => {
+      const amenitiesList = Object.values(selectedAmenities).join(', ');
+      $('.popover h4').text(amenitiesList);
+    };
+  
+    $.getJSON('http://0.0.0.0:5001/api/v1/status/', (data) => {
+      if (data.status === 'OK') {
+        $('#api_status').addClass('available');
+      } else {
+        $('#api_status').removeClass('available');
       }
     });
+  
+    $('button').click(function () {
+      $.ajax({
+        type: 'POST',
+        url: 'http://0.0.0.0:5001/api/v1/places_search/',
+        contentType: 'application/json',
+        data: JSON.stringify({ amenities: Object.keys(selectedAmenities) }),
+        success: (data) => {
+          $('section.places').empty();
+          for (const place of data) {
+            $('section.places').append(`
+              <article>
+                <div class="title_box">
+                  <h2>${place.name}</h2>
+                  <div class="price_by_night">$${place.price_by_night}</div>
+                </div>
+                <div class="information">
+                  <div class="max_guest">${place.max_guest} Guest${place.max_guest !== 1 ? 's' : ''}</div>
+                  <div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms !== 1 ? 's' : ''}</div>
+                  <div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms !== 1 ? 's' : ''}</div>
+                </div>
+                <div class="description">
+                  ${place.description}
+                </div>
+              </article>
+            `);
+          }
+        }
+      });
+    });
   });
-});
